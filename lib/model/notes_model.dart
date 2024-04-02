6f877c96885f2f08061fd7a5b4f0ce4fa00fa2de
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2024-04-02 13:24:32 +0530

    updated

--- lib/model/notes_model.dart
+++ lib/model/notes_model.dart
@@ -5,7 +5,7 @@ class NotesModel {
   NotesModel({required this.title, required this.notes, this.id});
 
   factory NotesModel.fromJson(String id, Map json) {
-    return NotesModel(title: 'title', notes: 'notes');
+    return NotesModel(title: json['title'], notes: json['notes'], id: id);
   }
   toJson() {
     return {'title': title, 'notes': notes, 'id': id};
